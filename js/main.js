c9d9396d7f354d009681d244c6bd938fb1d5ee88
Ageu Assunção Simões Galindo <ageuassuncaosimoesgalindo@gmail.com>
2021-12-08 07:26:35 -0300

    version 0.1

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ let units = {
 	}
 }
 
-const requestUrl = "https://github.com/Bulovask/conversor/js/units.json";
+const requestUrl = "https://github.com/Bulovask/conversor/blob/master/js/units.json";
 const request = new XMLHttpRequest();
 request.open("GET", requestUrl);
 request.responseType = "json";
